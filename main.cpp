b0a416934ad407ad3c3ed635f489f2fbb31a1e4c
Briiskle <rebecca.ramos237@gmail.com>
2025-09-07 23:43:58 +0000

    Edited Code

--- main.cpp
+++ main.cpp
@@ -2,8 +2,18 @@
 using namespace std;
 
 int main() {
+//Define//
   const double length = 7.2;
   const double width = 1.3;
+//Calculate Perimeter//
+  double perimeter = 2 * length + 2 * width;
+//Calculate Area//
+  double area = length * width;
+//Print!//
+  cout<< "Rectangle Properties: \nLength: " <<length<<'.';
+  cout<< "\nWidth: " <<width<<'.';
+  cout<< "\nArea: " <<area<<'.';
+  cout<< "\nPerimeter: " <<perimeter<<'.'<<endl;
 
 
 }
